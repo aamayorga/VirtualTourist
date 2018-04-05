388467875f148c1d0b3952edaff771a7e8d503f3
aamayorga <amayorga95@outlook.com>
2018-04-05 08:04:37 -0700

    feat: Delete Pins When user presses the Edit button he will enter delete mode where any pin he touches on the map will be deleted from the map view and Core Data.

--- VirtualTourist/Controller/TravelLocationsViewController.swift
+++ VirtualTourist/Controller/TravelLocationsViewController.swift
@@ -14,56 +14,114 @@ class TravelLocationsViewController: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
     
+    var deleteMode = false
     var dataController: DataController!
-    var pins: [Pin] = []
+    var fetchedResultsController:NSFetchedResultsController<Pin>!
+    
+    func setUpFetchResultsController() {
+        let fetchRequest:NSFetchRequest<Pin> = Pin.fetchRequest()
+        let sortDesc = NSSortDescriptor(key: "longitude", ascending: false)
+        fetchRequest.sortDescriptors = [sortDesc]
+        
+        fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: dataController.viewContext, sectionNameKeyPath: nil, cacheName: nil)
+        fetchedResultsController.delegate = self
+        
+        do {
+            try fetchedResultsController.performFetch()
+        } catch {
+            fatalError("The fetch could not be performed: \(error.localizedDescription)")
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         mapView.delegate = self
+        setUpFetchResultsController()
+        addPinsToMap(fetchedResultsController.fetchedObjects!)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         
-        let fetchRequest:NSFetchRequest<Pin> = Pin.fetchRequest()
-        if let result = try? dataController.viewContext.fetch(fetchRequest) {
-            pins = result
-            addPinsToMap(pinsArray: pins)
+        setUpFetchResultsController()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        fetchedResultsController = nil
+    }
+    
+    // MARK: Helper
+    
+    func goToPhotoAlbumView(annotation: MKAnnotation) {
+        let photoAlbumVC = storyboard?.instantiateViewController(withIdentifier: "photoAlbumVC") as! PhotoAlbumViewController
+        photoAlbumVC.annotation = annotation
+        
+        navigationController?.pushViewController(photoAlbumVC, animated: true)
+    }
+    
+    func addPinsToMap(_ pinsArray: [Pin]) {
+        for pin in pinsArray {
+            let annotation = MKPointIDAnnotation()
+            annotation.coordinate = CLLocationCoordinate2DMake(pin.latitude, pin.longitude)
+            annotation.id = pin.objectID
+            mapView.addAnnotation(annotation)
         }
     }
     
-    // MARK: Editing
-    
-    func savePin(coords: CLLocationCoordinate2D) {
-        let pin = Pin(context: dataController.viewContext)
-        pin.latitude = coords.latitude
-        pin.longitude = coords.longitude
-        
-        do {
-            try dataController.viewContext.save()
-        } catch {
-            print("Failed to save data")
-        }
-    }
-    
-    // MARK: Add/Remove Pins
-    @IBAction func editPins(_ sender: UIBarButtonItem) {
-        print("Edit Button tapped")
-    }
-    
+    // MARK: Saving Pins
     @IBAction func placePin(_ sender: UILongPressGestureRecognizer) {
         if sender.state != UIGestureRecognizerState.began { return }
         
         let touchLocation = sender.location(in: mapView)
         let locationCoordinate = mapView.convert(touchLocation, toCoordinateFrom: mapView)
-        let annotation = MKPointAnnotation()
+        let pointAnnotation = MKPointIDAnnotation()
         
-        annotation.coordinate = locationCoordinate
-        self.mapView.addAnnotation(annotation)
-        savePin(coords: locationCoordinate)
+        pointAnnotation.coordinate = locationCoordinate
+        
+        savePin(pointAnnotation)
+        
+        mapView.addAnnotation(pointAnnotation)
     }
     
-    func addPinsToMap(pinsArray: [Pin]) {
-        for pin in pinsArray {
-            let annotation = MKPointAnnotation()
-            annotation.coordinate = CLLocationCoordinate2DMake(pin.latitude, pin.longitude)
-            mapView.addAnnotation(annotation)
+    func savePin(_ point: MKPointIDAnnotation) {
+        let pin = Pin(context: dataController.viewContext)
+        pin.latitude = point.coordinate.latitude
+        pin.longitude = point.coordinate.longitude
+        
+        do {
+            try dataController.viewContext.save()
+            
+            point.id = pin.objectID    // ID will not be the same if set before context is saved
+        } catch {
+            print("Failed to save data")
+        }
+    }
+    
+    
+    // MARK: Removing Pins
+    
+    @IBAction func editPins(_ sender: UIBarButtonItem) {
+        print("Edit Button tapped")
+        deleteMode = !deleteMode
+    }
+    
+    func deletePin(_ annotationView: MKAnnotationView) {
+        guard let pointAnnotation = annotationView.annotation as! MKPointIDAnnotation? else {
+            print("Couldn't type cast MKAnnotationView's annotation to Point Annotation")
+            return
+        }
+        
+        let pinToDelete = fetchedResultsController.managedObjectContext.object(with: pointAnnotation.id)
+        dataController.viewContext.delete(pinToDelete)
+
+        do {
+            try dataController.viewContext.save()
+            mapView.removeAnnotation(annotationView.annotation!)
+        } catch {
+            print("Failed to save (deleted) data")
         }
     }
 }
@@ -72,13 +130,24 @@ class TravelLocationsViewController: UIViewController {
 extension TravelLocationsViewController: MKMapViewDelegate {
     
     func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
-        let photoAlbumVC = storyboard?.instantiateViewController(withIdentifier: "photoAlbumVC") as! PhotoAlbumViewController
-        
-        // Add annotation
-        let annotation = MKPointAnnotation()
-        annotation.coordinate = view.annotation!.coordinate
-        photoAlbumVC.annotation = annotation
-        
-        navigationController?.pushViewController(photoAlbumVC, animated: true)
+        if deleteMode {
+            deletePin(view)
+        } else {
+            goToPhotoAlbumView(annotation: view.annotation!)
+        }
+    }
+}
+
+// MARK: Fetched Result Controller Delegate Methods
+extension TravelLocationsViewController: NSFetchedResultsControllerDelegate {
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        switch type {
+        case .insert:
+            break
+        case .delete:
+            break
+        default:
+            fatalError("Unsupported behavior called")
+        }
     }
 }
---
+++ VirtualTourist/Model/MKPointIDAnnotation.swift
@@ -0,0 +1,14 @@
+//
+//  MKPointIDAnnotation.swift
+//  VirtualTourist
+//
+//  Created by Ansuke on 4/5/18.
+//  Copyright © 2018 AM. All rights reserved.
+//
+
+import MapKit
+import CoreData
+
+class MKPointIDAnnotation: MKPointAnnotation {
+    var id = NSManagedObjectID()
+}
