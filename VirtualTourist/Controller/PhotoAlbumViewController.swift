6398d7d932f8fc89a18a2ce414731fef810f1d34
aamayorga <amayorga95@outlook.com>
2018-04-06 06:58:43 -0700

    feat: Set up collection flow layout
    Set up the collection view controller flow layout to put 3 images per
    line and have appropriate spacing inbetween pictures.

--- VirtualTourist/Controller/PhotoAlbumViewController.swift
+++ VirtualTourist/Controller/PhotoAlbumViewController.swift
@@ -9,19 +9,35 @@
 import UIKit
 import MapKit
 
+private let reuseIdentifier = "photoCellReuseIdentifier"
+
 class PhotoAlbumViewController: UIViewController {
     
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var noImagesLabel: UILabel!
     @IBOutlet weak var photoCollectionView: UICollectionView!
+    @IBOutlet weak var newCollectionBarButton: UIBarButtonItem!
+    @IBOutlet weak var collectionViewFlowLayout: UICollectionViewFlowLayout!
     
     var annotation: MKAnnotation!
+    
+    var photos = [#imageLiteral(resourceName: "loading"), #imageLiteral(resourceName: "loading"), #imageLiteral(resourceName: "loading"), #imageLiteral(resourceName: "loading"), #imageLiteral(resourceName: "loading"), #imageLiteral(resourceName: "loading"), #imageLiteral(resourceName: "loading"), #imageLiteral(resourceName: "loading"), #imageLiteral(resourceName: "loading")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        photoCollectionView.dataSource = self
+        
         mapView.addAnnotation(annotation)
         mapView.isUserInteractionEnabled = false
         mapView.setRegion(MKCoordinateRegionMake(annotation.coordinate, MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)), animated: false)
+        
+        let space: CGFloat = 3.0
+        let dimension = (photoCollectionView.frame.size.width - (2 * space)) / 3.0
+        
+        collectionViewFlowLayout.minimumInteritemSpacing = space
+        collectionViewFlowLayout.minimumLineSpacing = space
+        collectionViewFlowLayout.itemSize = CGSize(width: dimension, height: dimension)
     }
     
     @IBAction func getNewCollectionOfPhotos(_ sender: UIBarButtonItem) {
@@ -38,4 +54,20 @@ class PhotoAlbumViewController: UIViewController {
     }
     */
 
+}
+
+extension PhotoAlbumViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photos.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! PhotoCollectionViewCell
+        let photo = photos[indexPath.row]
+        
+        cell.imageView.image = photo
+        cell.imageView.backgroundColor = UIColor.black
+        
+        return cell
+    }
 }
---
+++ VirtualTourist/Model/PhotoCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCollectionViewCell.swift
+//  VirtualTourist
+//
+//  Created by Ansuke on 4/6/18.
+//  Copyright © 2018 AM. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+}
